419a453c5c1b6f3c4d74fecd5cef17565bb7008d
Lj Miranda <ljvmiranda@gmail.com>
2024-07-28 19:27:31 -0700

    Fix minor changes from previous preferences

--- scripts/run_generative.py
+++ scripts/run_generative.py
@@ -180,7 +180,7 @@ def main():
 
         def get_judgement(batch, debug=args.debug):
             mult_turn = True if len(batch["text_chosen"]) > 2 else False
-            prompt = batch["prompt"]
+            prompt = batch["text_chosen"][0]["content"]
             answer_a = batch["text_chosen"]
             answer_b = batch["text_rejected"]
 
@@ -251,7 +251,7 @@ def main():
         def format_judgements(batch, optional_chat_template=None):
             # TODO expand this to include fastchat chat templates if needed
             mult_turn = True if len(batch["text_chosen"]) > 2 else False
-            prompt = batch["prompt"]
+            prompt = batch["text_chosen"][0]["content"]
             answer_a = batch["text_chosen"]
             answer_b = batch["text_rejected"]
 
