705f324e6f5ae242fc9d3922539844141e514021
ljvmiranda921 <ljvmiranda@gmail.com>
2024-08-16 19:23:51 -0700

    [wip] Update

--- scripts/get_human_eval.py
+++ scripts/get_human_eval.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import logging
 
 import pandas as pd
+from pycm import ConfusionMatrix
 
 logging.basicConfig(level=logging.INFO)
 
@@ -12,6 +13,7 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--reference_path", type=Path, help="Path to the reference containing the 'gold' preferences.")
     parser.add_argument("--annotation_path", type=Path, help="Path to the annotations file.")
+    parser.add_argument("--dropna", default=False, action="store_true", help="Drop instances with no annotations")
     # fmt: on
     return parser.parse_args()
 
@@ -25,15 +27,25 @@ def main():
     reference["gold_preference"] = "A"
     annotations = annotations[["id", "human_preference", "notes"]]
 
-    # Combine in single dataframe and bring back random swaps
+    # Combine in single dataframe and apply random swaps
+    # We swap the gold preferences because if they're all the same value,
+    # it affects the random-corrected chance in the IAA measures
     df = pd.merge(reference, annotations, on="id")
-    df["human_preference"] = df.apply(
-        lambda row: (
-            "B"
-            if row["human_preference"] == "A" and row["swapped"] == 1
-            else "A" if row["human_preference"] == "B" and row["swapped"] == 1 else row["human_preference"]
-        ),
-        axis=1,
+    df["gold_preference"] = df.apply(lambda row: "B" if row["swapped"] == 1 else "A", axis=1)
+    if args.dropna:
+        df = df.dropna(subset=["human_preference"])
+        logging.info(f"Dropped instances with no annotations. No. of instances: {len(df)}")
+
+    cm = ConfusionMatrix(
+        actual_vector=df["gold_preference"].to_list(),
+        predict_vector=df["human_preference"].to_list(),
+    )
+    print(
+        f" Overall accuracy: {cm.Overall_ACC}\n",
+        f"Per-class accuracy: {cm.ACC}\n",
+        f"Cohen's Kappa: {cm.Kappa}\n",
+        f"Krippendorff Alpha: {cm.Alpha}\n",
+        f"Gwet's AC1: {cm.AC1}\n",
     )
 
 
