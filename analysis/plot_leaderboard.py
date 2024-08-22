a4da3c523a22a7ad042c3d8c54dab45786285aeb
Lj Miranda <ljvmiranda@gmail.com>
2024-08-22 15:15:08 -0700

    [wip] Update

--- analysis/plot_leaderboard.py
+++ analysis/plot_leaderboard.py
@@ -1,7 +1,11 @@
 import argparse
 from pathlib import Path
 
+import pandas as pd
 import matplotlib.pyplot as plt
+from huggingface_hub import snapshot_download
+
+from analysis.plot_utils import get_scores
 
 
 def get_args():
@@ -16,6 +20,16 @@ def get_args():
 
 def main():
     args = get_args()
+    dataset_dir = Path(snapshot_download(args.dataset, repo_type="dataset", force_download=args.force_download))
+    lang_folders = [d for d in dataset_dir.iterdir() if d.is_dir()]
+
+    lang_scores = {}
+    for lang_dir in lang_folders:
+        model_scores = get_scores(lang_dir)
+        lang_scores[lang_dir.name] = {score["model"]: score["score"] for score in model_scores}
+
+    lang_scores_df = pd.DataFrame(lang_scores)
+    breakpoint()
 
 
 if __name__ == "__main__":
