6821adb2915c16466b39c222ab80e2bba27b3b5f
ljvmiranda921 <ljvmiranda@gmail.com>
2024-08-23 16:26:31 -0700

    [wip] Update

---
+++ scripts/convert_orig_scores.py
@@ -0,0 +1,11 @@
+import argparse
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+def get_args():
+    # fmt: off
+    parser = argparse.ArgumentParser(description="Convert RewardBench scores to our scores")
+    # fmt: on
+    pass
