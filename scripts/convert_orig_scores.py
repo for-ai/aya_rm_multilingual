8c857e9351f2960a8c5d97804bbf7f32f4f519ec
ljvmiranda921 <ljvmiranda@gmail.com>
2024-08-23 16:28:21 -0700

    [wip] Update

--- scripts/convert_orig_scores.py
+++ scripts/convert_orig_scores.py
@@ -6,6 +6,13 @@ logging.basicConfig(level=logging.INFO)
 
 def get_args():
     # fmt: off
-    parser = argparse.ArgumentParser(description="Convert RewardBench scores to our scores")
+    parser = argparse.ArgumentParser(description="Convert RewardBench scores to our formatting")
+    parser.add_argument("--filepath", required=True, help="Filepath of JSON to download.")
+    parser.add_argument("--output_file", required=True, help="Filepath to save the updated JSON file.")
+    parser.add_argument("--dataset_name", default="allenai/reward-bench", help="HuggingFace dataset to obtain datasets from.")
     # fmt: on
+    return parser.parse_args()
+
+
+def main():
     pass
